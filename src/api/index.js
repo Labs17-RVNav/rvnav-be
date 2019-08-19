d3fecdacaae8341b1c784073e2cc79fd024c2a87
Kevin Sims <kevinsims1@outlook.com>
2019-08-19 13:28:00 -0700

    testing enviorment added using jest

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-const emojis = require('./emojis');
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -10,6 +8,5 @@ router.get('/', (req, res) => {
   });
 });
 
-router.use('/emojis', emojis);
 
 module.exports = router;

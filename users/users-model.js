1b2f7646c8ddfd50a9f12b6b23683a690cebaf53
wcolts2000 <wcolts2000@gmail.com>
2019-08-19 17:07:05 -0700

    modified dbconfig import to reflect filename

--- users/users-model.js
+++ users/users-model.js
@@ -1,30 +1,28 @@
-const db = require('../database/dbConfig.js');
+const db = require('../database/dbconfig.js');
 
 module.exports = {
-    add,
-    findById,
-    findBy,
-    findUsers
+  add,
+  findById,
+  findBy,
+  findUsers
+};
+
+async function add(user) {
+  const [id] = await db('users').insert(user, 'id');
+
+  return findById(id);
 }
 
-
-async function add(user){
-    const [id] = await db('users').insert(user, 'id')
-
-    return findById(id)
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
 }
 
-function findById(id){
-    return db('users')
-        .where({id})
-        .first()
-}
-
-
-function findBy(filter){
-    return db('users').where(filter)
+function findBy(filter) {
+  return db('users').where(filter);
 }
 
 function findUsers() {
-    return db('users')
+  return db('users');
 }

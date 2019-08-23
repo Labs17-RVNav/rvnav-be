773d7ab766b3ea3c47e902eb2fe2d5187066823c
wcolts2000 <wcolts2000@gmail.com>
2019-08-22 17:01:30 -0700

    db table migrations

--- database/migrations/20190819134918_rvlife.js
+++ database/migrations/20190819134918_rvlife.js
@@ -1,19 +1,22 @@
-
 exports.up = function(knex) {
-    return knex.schema.createTable('users', users => {
-        users.increments();
-    
-        users
-          .string('username', 128)
-          .notNullable()
-          .unique();
-        users.string('first_name', 120).notNullable()
-        users.string('last_name', 128).notNullable()
-        users.string('password', 128).notNullable();
-        users.string('vehicle_class', 128)
-      })
+  return knex.schema.createTable('users', users => {
+    users.increments();
+
+    users
+      .string('username', 128)
+      .notNullable()
+      .unique();
+    users.string('first_name', 120).notNullable();
+    users.string('last_name', 128).notNullable();
+    users.string('password', 128).notNullable();
+    users
+      .string('email', 128)
+      .notNullable()
+      .unique();
+    users.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('users');
+  return knex.schema.dropTableIfExists('users');
 };
---
+++ database/migrations/20190822152156_vehicle.js
@@ -0,0 +1,26 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('vehicle', tbl => {
+    tbl.increments();
+    tbl
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl.float('height');
+    tbl.float('width');
+    tbl.float('length');
+    tbl.float('weight');
+    tbl.integer('axel_count');
+    tbl.string('class', 255);
+    tbl.timestamps(true, true);
+    tbl.boolean('dual_tires');
+    tbl.boolean('trailer');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('vehicle');
+};
